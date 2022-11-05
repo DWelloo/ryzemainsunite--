5495bcd04404ead7ec00636421334ea978a4e411
TheUmbralPyre <theumbralpyre@gmail.com>
2022-11-05 09:19:22 +0200

    Fix 42% Success Rate on Translations

--- ryze_main_converter.py
+++ ryze_main_converter.py
@@ -4,7 +4,7 @@ import time
 englishText = input()
 
 # Convert the English Text to the Ryze Language
-ryzeText = englishText.replace("e", "e(q)").replace("E", "E(Q)").replace("q", "(e)q").replace("Q", "(E)Q")
+ryzeText = englishText.replace("q", "(e)q").replace("Q", "(E)Q").replace("e", "e(q)").replace("E", "E(Q)")
 
 # Print the Ryze Language Text to the User
 print(ryzeText)
